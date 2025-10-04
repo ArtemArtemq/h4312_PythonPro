a6c1f1c19ca214d5c5a040d6686b36d82a59647a
ArtemArtemq <artempuskar@gmail.com>
2025-10-04 11:00:22 +0300

    dz 4

---
+++ Dz/dz4.py
@@ -0,0 +1,15 @@
+class Human:
+    def __init__(self, name, age, height, money):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.money = money
+
+class Children(Human):
+    pass
+
+class Student(Human):
+    pass
+
+class Worker(Human):
+    pass
