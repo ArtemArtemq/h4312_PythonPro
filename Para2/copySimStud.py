68f99b077ec2ac720f7ac6bbb5dd03dddfd4db36
ArtemArtemq <artempuskar@gmail.com>
2025-10-11 12:02:04 +0300

    dz 8

--- Para2/copySimStud.py
+++ Para2/copySimStud.py
@@ -1,4 +1,9 @@
 from random import randint
+import  logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename="Sims_logs",
+                    filemode="w")
 
 class Student:
     def __init__(self, name):
@@ -7,36 +12,36 @@ class Student:
         self.progress = 0
         self.alive = True
     def to_study(self):
-        print("Time to study")
+        logging.debug("Time to study")
         self.progress += 0.11
         self.gladness -= 3
     def to_study_in_itStep(self):
-        print("Time to study in itStep")
+        logging.debug("Time to study in itStep")
         self.progress += 0.21
         self.gladness -= 6.1
     def to_sleep(self):
-        print("Time to sleep")
+        logging.debug("Time to sleep")
         self.gladness += 2.1
     def to_chill(self):
-        print("Rest time")
+        logging.debug("Rest time")
         self.gladness += 4.5
         self.progress -= 0.1
     def is_alive(self):
         if self.progress < -0.5:
-            print("Cast out")
+            logging.debug("Cast out")
             self.alive = False
         elif self.gladness <= 0:
-            print("Depression")
+            logging.debug("Depression")
             self.alive = False
         elif self.progress > 5:
-            print("Passed externally")
+            logging.debug("Passed externally")
             self.alive = False
     def end_of_day(self):
-        print(f"Gladness = {self.gladness}")
-        print(f"Progress = {round(self.progress, 2)}")
+        logging.debug(f"Gladness = {self.gladness}")
+        logging.debug(f"Progress = {round(self.progress, 2)}")
     def live(self, day):
         text_day = f"Day {day} of {self.name} live"
-        print(f"{text_day:=^30}")
+        logging.debug(f"{text_day}:=^30")
         cube = randint(1, 4)
         if cube == 1:
             self.to_sleep()
